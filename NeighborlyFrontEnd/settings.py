fb3dc121f2cb62acb38d47e3a0e3d854bbd073fd
dasha.dubinovska <daria.dubinovska@amadeus.com>
2021-02-18 19:29:05 +0100

    Event hub trigger

--- NeighborlyFrontEnd/settings.py
+++ NeighborlyFrontEnd/settings.py
@@ -1,14 +1,14 @@
 #--------- Flask settings
-SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
+SERVER_HOST = "https://myneighborlyapiv1.azurewebsites.net" #'0.0.0.0' # Update this for the appropriate front-end website when up
 SERVER_PORT = 5000
 FLASK_DEBUG = True # Do not use debug mode in prod
 
 # Flask-Restplus settings
-SWAGGER_UI_DOC_EXPANSION = 'list'
-RESTPLUS_VALIDATE = True
-RESTPLUS_MASK_SWAGGER = False
-RESTPLUS_404_HELP = True
-API_VERSION = 'v1'
+#SWAGGER_UI_DOC_EXPANSION = 'list'
+#RESTPLUS_VALIDATE = True
+#RESTPLUS_MASK_SWAGGER = False
+#RESTPLUS_404_HELP = True
+#API_VERSION = 'v1'
 
 #-------- Azure constants
 
